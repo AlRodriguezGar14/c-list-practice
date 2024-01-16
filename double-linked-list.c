2d90966ee226ca9f30d781f80ace4e9913a737e3
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-01-16 03:59:33 +0100

    sorting algorithm apparently working. Not sure about the  number of iterations

--- double-linked-list.c
+++ double-linked-list.c
@@ -6,7 +6,7 @@
 /*   By: alberrod <alberrod@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 02:38:47 by alberrod          #+#    #+#             */
-/*   Updated: 2024/01/14 13:14:50 by alberrod         ###   ########.fr       */
+/*   Updated: 2024/01/16 03:58:09 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -191,70 +191,127 @@ void	reverse_rotation(t_dll *bucket)
 	tmp->prev = bucket->tail;
 }
 
-int	main(void)
+void	sort(t_dll *a)
 {
-	t_dll	*stack_a;
-	t_dll	*stack_b;
-	int		idx;
+	t_dll	*b;
+	t_node	*pivot;
+	int		iteration;
 
-	stack_a = new_dll();
-	stack_b = new_dll();
-	if (!stack_a)
-		exit(EXIT_FAILURE);
-	idx = 0;
-	while (idx < 10 )
+	b = new_dll();
+	iteration = 0;
+	while (a->len > 0)
 	{
-		append(stack_a, idx);
-		idx++;
+		pivot = new_node(a->head->value);
+		pull(a);
+		while (b->len && b->head->value > pivot->value)
+		{
+			push_to(b, a);
+			ft_printf("b to a\n");
+			iteration++;
+		}
+		// push_to(a, b);
+		push(b, pivot->value);
+		ft_printf("a to b\n");
+		iteration++;
 	}
-	idx = 0;
-	// swap(stack_a);
-	// pull(stack_a);
-	// swap(stack_a);
-	// push_to(stack_a, stack_b);
-	// push_to(stack_a, stack_b);
-	// push_to(stack_a, stack_b);
-	// pull(stack_a);
-	// pull(stack_a);
-	// pull(stack_a);
-	// pull(stack_a);
-	// pull(stack_a);
-	// push(stack_a, 42);
-	while (idx < stack_a->len)
+	while (b->len)
 	{
-		// pull(stack_a);
-		// append(stack_a, 2);
-		// ft_printf("nbr a: %d |", stack_a->head->value);
-		// stack_a->head = stack_a->head->next;
-		// ft_printf("| nbr b: %d\n", stack_b->head->value);
-		// stack_b->head = stack_b->head->next;
-		// ft_printf("nbr a head: %d\n", stack_a->head->value);
-		// stack_a->head = stack_a->head->next;
-		idx++;
+		push_to(b, a);
+		ft_printf("a to b\n");
+		iteration++;
 	}
-	ft_printf("\n=====\n");
-	push_to(stack_a, stack_b);
-	push_to(stack_a, stack_b);
-	push_to(stack_a, stack_b);
-	push_to(stack_a, stack_b);
-	push_to(stack_a, stack_b);
-	rotation(stack_b);
-	rotation(stack_b);
-	reverse_rotation(stack_b);
-	reverse_rotation(stack_b);
-	// rotation(stack_b);
-	idx = 0;	
-	while (idx < stack_b->len)
-	{
-		ft_printf("nbr b: %d\n", stack_b->head->value);
-		stack_b->head = stack_b->head->next;
-		idx++;
-	}
-  while (stack_a->len)
-    pull(stack_a);
-  while (stack_b->len)
-    pull(stack_b);
-  free(stack_a);
-  free(stack_b);
-	return (EXIT_SUCCESS);
+	ft_printf("iterations: %d\n", iteration);
 }
+
+int main()
+{
+	t_dll	*a;
+
+	a = new_dll();
+	append(a, 50);
+	append(a, 500);
+	append(a, 5);
+	append(a, 10);
+	append(a, 42);
+	append(a, 24);
+	append(a, 542);
+	sort(a);
+	int idx = 0;
+	t_node *curr = a->head;
+	while (idx < a->len)
+	{
+		printf("%d ", curr->value);
+		curr = curr->next;
+		idx++;
+	}
+
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	t_dll	*stack_a;
+// 	t_dll	*stack_b;
+// 	int		idx;
+
+// 	stack_a = new_dll();
+// 	stack_b = new_dll();
+// 	if (!stack_a)
+// 		exit(EXIT_FAILURE);
+// 	idx = 0;
+// 	while (idx < 10 )
+// 	{
+// 		append(stack_a, idx);
+// 		idx++;
+// 	}
+// 	idx = 0;
+// 	// swap(stack_a);
+// 	// pull(stack_a);
+// 	// swap(stack_a);
+// 	// push_to(stack_a, stack_b);
+// 	// push_to(stack_a, stack_b);
+// 	// push_to(stack_a, stack_b);
+// 	// pull(stack_a);
+// 	// pull(stack_a);
+// 	// pull(stack_a);
+// 	// pull(stack_a);
+// 	// pull(stack_a);
+// 	// push(stack_a, 42);
+// 	while (idx < stack_a->len)
+// 	{
+// 		// pull(stack_a);
+// 		// append(stack_a, 2);
+// 		// ft_printf("nbr a: %d |", stack_a->head->value);
+// 		// stack_a->head = stack_a->head->next;
+// 		// ft_printf("| nbr b: %d\n", stack_b->head->value);
+// 		// stack_b->head = stack_b->head->next;
+// 		// ft_printf("nbr a head: %d\n", stack_a->head->value);
+// 		// stack_a->head = stack_a->head->next;
+// 		idx++;
+// 	}
+// 	ft_printf("\n=====\n");
+// 	push_to(stack_a, stack_b);
+// 	push_to(stack_a, stack_b);
+// 	push_to(stack_a, stack_b);
+// 	push_to(stack_a, stack_b);
+// 	push_to(stack_a, stack_b);
+// 	rotation(stack_b);
+// 	rotation(stack_b);
+// 	reverse_rotation(stack_b);
+// 	reverse_rotation(stack_b);
+// 	// rotation(stack_b);
+// 	idx = 0;	
+// 	while (idx < stack_b->len)
+// 	{
+// 		ft_printf("nbr b: %d\n", stack_b->head->value);
+// 		stack_b->head = stack_b->head->next;
+// 		idx++;
+// 	}
+//   while (stack_a->len)
+//     pull(stack_a);
+//   while (stack_b->len)
+//     pull(stack_b);
+//   free(stack_a);
+//   free(stack_b);
+// 	return (EXIT_SUCCESS);
+// }
